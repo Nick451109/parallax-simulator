184f91cec02b12516c52af05c122516ce8aa4ae2
Juan Saeteros <jumasaet@espol.edu.ec>
2025-09-08 09:37:43 -0500

    sharelink fixed

--- simulate_parallax.py
+++ simulate_parallax.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
     demo.launch(
         server_name="127.0.0.1",
         server_port=7860,
-        share=True,
+        share=False,
         show_error=True
     )
 
